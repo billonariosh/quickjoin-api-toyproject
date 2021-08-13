f61f9888e925dd18e81c5749cad2ddc3c5e488eb
DESKTOP-3HMTLGG\billo <billonariosh@gamil.com>
2021-08-14 01:12:17 +0900

    2021.08.14(토) 하이버네이트 + JPA 첫 공부 시작. 신세계 그자체 ;;; 와우

--- src/test/java/com/billo/quickjoin/QuickjoinApplicationTests.java
+++ src/test/java/com/billo/quickjoin/QuickjoinApplicationTests.java
@@ -1,6 +1,9 @@
 package com.billo.quickjoin;
 
+import com.billo.entity.User;
+import com.billo.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +12,4 @@ class QuickjoinApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
